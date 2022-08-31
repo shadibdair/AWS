093107b8b37dc407dceb2fdc8ffe9efb2915ce17
shadibdair <bdairshadi@gmail.com>
2022-08-31 20:18:40 +0300

    Done with main fun

--- boto3/venv/main.py
+++ boto3/venv/main.py
@@ -3,4 +3,10 @@ from create_user import cr_user as c_u
 from create_group import cr_group as c_g
 
 def main():
-    
+    groupname = input("Enter name of the new group: ")
+    c_g(groupname)
+    print("GroupName is: " + groupname)
+
+    username = input("Enter name of the new user: ")
+    c_u(username)
+    print("GroupName is: " + username)
