3da2ccdaeca4e1e083ec7d24bb43976fb6853403
bdairshadi <bdairshadi@gmail.com>
2022-09-30 22:36:11 +0300

    Created script that list files from s3 and hash it

---
+++ DynamoDB/hasher/s3_list_bucket_files.py
@@ -0,0 +1,26 @@
+from boto3 import client
+import hashlib
+
+hasher = hashlib.sha256()
+
+conn = client('s3')  # again assumes boto.cfg setup, assume AWS S3
+for key in conn.list_objects(Bucket='mainbucketwithouthash')['Contents']:
+    #print(key['Key'])
+    file = key['Key']
+    # Hash
+    f = open(file, "rb")
+    text = f.read()
+    #print(file)
+    hasher.update(text)
+    print(hasher.digest(),"\n\n")
+    
+
+
+
+# Output:
+# text1.txt
+# text2.txt
+# text3.txt
+# text4.txt
+# text5.txt
+# text6.txt
